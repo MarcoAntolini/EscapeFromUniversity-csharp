42c11286079408cf328dbd9f247d7dd3f0d66d1e
Denis Caushaj <denis.caushaj@studio.unibo.it>
2022-07-11 13:53:29 +0200

    Fix syntax errors

--- Caushaj/main/MapPropertiesImpl.cs
+++ Caushaj/main/MapPropertiesImpl.cs
@@ -4,23 +4,39 @@ using System.Collections.Generic;
 namespace main;
 
 /// <summary>
-/// A class which models a map.
+/// A class which implements MapProperties.
 /// </summary>
-public class MapPropertiesImpl
+public class MapPropertiesImpl : MapProperties
 {
-    private int _width;
-    private int _height;
-    private int _tileWidth;
-    private int _tileHeight;
-    private List<Tileset> _tilesets;
+    /// <summary>
+    /// A constructor for MapPropertiesImpl.
+    /// </summary>
+    /// <param name="width">the width of the map</param>
+    /// <param name="height">the height of the map</param>
+    /// <param name="tileWidth">the width of a tile</param>
+    /// <param name="tileHeight">the height of a tile</param>
+    /// <param name="tilesets">the list of tilesets</param>
+    public MapPropertiesImpl(int width, int height, int tileWidth, int tileHeight, List<Tileset> tilesets) {
+        Width = width;
+        Height = height;
+        TileWidth = tileWidth;
+        TileHeight = tileHeight;
+        Tilesets = tilesets;
+    }
     
-    public MapProperties(int width, int height, int tileWidth, int tileHeight,
-        List<Layer> layers, List<Tileset> tilesets) {
-        _width = width;
-        _height = height;
-        _tileWidth = tileWidth;
-        _tileHeight = tileHeight;
-        _layers = layers;
-        _tilesets = tilesets;
+    public override int Width() { get; }
+
+    public override int Height() { get; }
+
+    public override int TileWidth() { get; }
+
+    public override int TileHeight() { get; }
+    
+    public override List<Tileset> Tilesets() { get; }
+
+    public override ToString()
+    {
+        return "MapProperties [width=" + width + ", height=" + height + ", tileWidth=" + tileWidth + ", tileHeight="
+               + tileHeight + ", tilesets=" + tilesets + "]";
     }
 }
