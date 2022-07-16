984bae66af4978de01e0f275222955b9f22a9df4
MarcoAntolini <marco.antolini2001@gmail.com>
2022-07-16 18:10:03 +0200

    create sprite animation class

---
+++ Antolini/SpriteAnimation.cs
@@ -0,0 +1,25 @@
+﻿namespace DefaultNamespace;
+
+public class SpriteAnimation
+{
+
+    private Rectangle _position;
+    public bool Visible { get; set; }
+    public readonly HitBox HitBox { get; }
+    public readonly Sprite Sprite { get; }
+    
+    public SpriteAnimation(Sprite sprite, HitBox hitBox) {
+        Sprite = sprite;
+        HitBox = hitBox;
+    }
+
+    public Rectangle GetPosition() {
+        return _position;
+    }
+
+    public void SetPosition(Point2D position) {
+        Point2D size = position.Sum(new Point2D(50, 57));
+        _position = new Rectangle(position, size);
+    }
+
+}
